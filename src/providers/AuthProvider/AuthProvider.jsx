35808ecef5e7189e7eccf72016aa3d9be735b373
zesanahmed <zesanahmed593@gmail.com>
2023-06-27 22:03:37 +0600

    google authentication added

--- src/providers/AuthProvider/AuthProvider.jsx
+++ src/providers/AuthProvider/AuthProvider.jsx
@@ -8,23 +8,28 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
     const signIn = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
     const logOut = () => {
+        setLoading(true);
         return signOut(auth);
     }
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, loggedUser => {
             console.log('logged in user inside auth state observer', loggedUser);
-            setUser(loggedUser)
+            setUser(loggedUser);
+            setLoading(false);
         })
         return () => {
             unsubscribe();
@@ -33,6 +38,7 @@ const AuthProvider = ({ children }) => {
 
     const authInfo = {
         user,
+        loading,
         createUser,
         signIn,
         logOut
--- src/routes/PrivateRoute/PrivateRoute.jsx
+++ src/routes/PrivateRoute/PrivateRoute.jsx
@@ -3,9 +3,16 @@ import { AuthContext } from '../../providers/AuthProvider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
     console.log(location);
+
+    if (loading) {
+        return <div className='text-center my-6'>
+            <span className="loading loading-spinner text-info "></span>
+        </div>
+    }
+
     if (user) {
         return children;
     }
